29000f8895347d329ec70f27118ceba8232c3e6f
NL-Nhat <nguyenlongnhat20050111@gmail.com>
2026-01-29 20:41:51 +0700

    bai 5

--- BT_Chuoi_C_Sharp/Bai4.cs
+++ BT_Chuoi_C_Sharp/Bai4.cs
@@ -1,50 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
+﻿//using System;
+//using System.Collections.Generic;
 
-namespace BT_Chuoi_C_Sharp
-{
-    internal class Bai4
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("=========================================");
-            Console.WriteLine("Bai 4: Nguyen Long Nhat 23115053122126 ||");
-            Console.WriteLine("=========================================\n");
+//namespace BT_Chuoi_C_Sharp
+//{
+//    internal class Bai4
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("=========================================");
+//            Console.WriteLine("Bai 4: Nguyen Long Nhat 23115053122126 ||");
+//            Console.WriteLine("=========================================\n");
 
-            List<int> arr = new List<int>();
-            int n;
-            int sum = 0;
+//            List<int> arr = new List<int>();
+//            int n;
+//            int sum = 0;
 
-            Console.Write("Nhap so phan tu: ");
-            while (!int.TryParse(Console.ReadLine(), out n))
-            {
-                Console.Write("Khong phai so, vui long nhap lai: ");
-            }
+//            Console.Write("Nhap so phan tu: ");
+//            while (!int.TryParse(Console.ReadLine(), out n))
+//            {
+//                Console.Write("Khong phai so, vui long nhap lai: ");
+//            }
 
-            for (int i = 0; i < n; i++)
-            {
-                int so;
-                Console.Write($"Nhap so thu {i + 1}: ");
-                while (!int.TryParse(Console.ReadLine(), out so))
-                {
-                    Console.Write("Khong phai so, vui long nhap lai: ");
-                }
-                arr.Add(so);
-            }
+//            for (int i = 0; i < n; i++)
+//            {
+//                int so;
+//                Console.Write($"Nhap so thu {i + 1}: ");
+//                while (!int.TryParse(Console.ReadLine(), out so))
+//                {
+//                    Console.Write("Khong phai so, vui long nhap lai: ");
+//                }
+//                arr.Add(so);
+//            }
 
-            if(arr.Count == 0)
-            {
-                Console.WriteLine("Danh sach rong.");
-                return;
-            }
+//            if(arr.Count == 0)
+//            {
+//                Console.WriteLine("Danh sach rong.");
+//                return;
+//            }
 
-            foreach (int x in arr)
-            {
-                sum += x;
-            }
+//            foreach (int x in arr)
+//            {
+//                sum += x;
+//            }
 
-            Console.WriteLine("Cac so da nhap: " + string.Join(" ", arr));
-            Console.WriteLine("Tong cac phan tu: " + sum);
-        }
-    }
-}
+//            Console.WriteLine("Cac so da nhap: " + string.Join(" ", arr));
+//            Console.WriteLine("Tong cac phan tu: " + sum);
+//        }
+//    }
+//}
