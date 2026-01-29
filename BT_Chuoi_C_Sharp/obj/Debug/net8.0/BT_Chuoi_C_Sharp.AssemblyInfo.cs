f4a92b8cb4b117ca13dd27d7e70a13773fb63093
NL-Nhat <nguyenlongnhat20050111@gmail.com>
2026-01-29 07:53:43 +0700

    Bai 1

--- BT_Chuoi_C_Sharp/obj/Debug/net8.0/BT_Chuoi_C_Sharp.AssemblyInfo.cs
+++ BT_Chuoi_C_Sharp/obj/Debug/net8.0/BT_Chuoi_C_Sharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BT_Chuoi_C_Sharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15e1a3a2c3f85b73df8f1df894fd16ee8c8c7496")]
 [assembly: System.Reflection.AssemblyProductAttribute("BT_Chuoi_C_Sharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BT_Chuoi_C_Sharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
